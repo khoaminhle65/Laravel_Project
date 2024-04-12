82ddd845d4f80465307c91cf6c971f9a94d96e57
kund20 <nguyentiendat6849@gmail.com>
2024-04-12 19:28:30 +0700

    update message

--- app/Http/Controllers/CrudUserController.php
+++ app/Http/Controllers/CrudUserController.php
@@ -17,47 +17,75 @@ class CrudUserController extends Controller
     /**
      * Login page
      */
- 
+    
 
     /**
      * User submit form login
      */
-
+    
 
     /**
      * Registration page
      */
-
+    
 
     /**
      * User submit form register
      */
-   
+    
 
     /**
      * View user detail page
      */
+   
 
     /**
      * Delete user by id
      */
+    
 
     /**
      * Form update user page
      */
-
+    public function updateUser(Request $request)
+    {
+        $user_id = $request->get('id');
+        $user = User::find($user_id);
+        
+        return view('crud_user.update', ['user' => $user]);
+    }
 
     /**
      * Submit form update user
      */
-   
+    public function postUpdateUser(Request $request)
+    {
+        $input = $request->all();
+
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,id,'.$input['id'],
+            'password' => 'required|min:6|same:password_confirmation',
+            'password_confirmation' => 'required|min:6'
+        ]);
+
+       $user = User::find($input['id']);
+       $user->name = $input['name'];
+       $user->email = $input['email'];
+       //$user->password = $input['password'];
+       $user->password = bcrypt($input['password']);
+       $user->save();
+        
+        return redirect("list")->withSuccess('You have signed-in');
+    }
 
     /**
      * List of users
      */
 
+
     /**
      * Sign out
      */
-
+    
 }
