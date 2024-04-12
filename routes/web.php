2920d8cd161c554e42ba7a650b4f3a4814aad615
kund20 <nguyentiendat6849@gmail.com>
2024-04-12 19:28:59 +0700

    update message

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
-use App\Http\Controllers\CrudUserController;
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CrudUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 
+Route::get('update', [CrudUserController::class, 'updateUser'])->name('user.updateUser');
+Route::post('update', [CrudUserController::class, 'postUpdateUser'])->name('user.postUpdateUser');
+
 Route::get('/', function () {
     return view('welcome');
 });
-
