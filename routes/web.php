f52f5d6a75068c20781c7f836f7c5552e1b6cbeb
DoTienDai <dotiendai789@gmail.com>
2024-04-12 18:52:54 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('list', [CrudUserController::class, 'listUser'])->name('user.list');
 
