d64a15b5e4b296cf6ea6820bbc093621e9b34c9a
kund20 <nguyentiendat6849@gmail.com>
2024-04-25 13:27:00 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,28 @@ use App\Http\Controllers\CrudUserController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('dashboard', [CrudUserController::class, 'dashboard']);
 
+Route::get('login', [CrudUserController::class, 'login'])->name('login');
+
+Route::post('login', [CrudUserController::class, 'authUser'])->name('user.authUser');
+
+Route::get('create', [CrudUserController::class, 'createUser'])->name('user.createUser');
+
+Route::post('create', [CrudUserController::class, 'postUser'])->name('user.postUser');
+
+Route::post('signOut', [CrudUserController::class, 'signOut'])->name('signOut');
+
+Route::get('read', [CrudUserController::class, 'readUser'])->name('user.readUser');
+
+Route::get('delete', [CrudUserController::class, 'deleteUser'])->name('user.deleteUser');
+
+Route::get('list', [CrudUserController::class, 'listUser'])->name('user.list');
 
 Route::get('update', [CrudUserController::class, 'updateUser'])->name('user.updateUser');
-Route::post('update', [CrudUserController::class, 'postUpdateUser'])->name('user.postUpdateUser');
 
+Route::post('update', [CrudUserController::class, 'postUpdateUser'])->name('user.postUpdateUser');
+Route::get('signOut', [CrudUserController::class, 'signOut'])->name('signout');
 Route::get('/', function () {
     return view('welcome');
 });
